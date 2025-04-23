7c975151e1063241a2b16647be0363cf2eb2f9f8
Nyan Oak Soe <nyanoaksoe77@gmail.com>
2025-04-23 13:32:39 +0630

    testing

--- app/Http/Requests/UpdateProgramRequest.php
+++ app/Http/Requests/UpdateProgramRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class UpdateProgramRequest extends FormRequest
 {
@@ -22,7 +24,24 @@ class UpdateProgramRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'uni_id' => 'sometimes|required|integer|exists:universities,id',
+            'name' => 'sometimes|required|string|max:255',
+            'detail' => 'sometimes|required|array',
+            'degree_type' => 'sometimes|required|string',
+            'duration' => 'sometimes|required|string|max:255',
+            'application_requirement' => 'sometimes|required|array',
+            'intake' => 'sometimes|required|string|max:255',
+            'payment_plan' => 'sometimes|required|in:monthly,per_semester,no_installements',
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => 'program-validation-fail',
+            'code' => 422,
+            'message' => 'Validation Error',
+            'data' => $validator->errors()
+        ], 422));
     }
 }
