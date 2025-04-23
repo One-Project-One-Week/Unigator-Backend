6126689659417a34320804cd9f15a6457a46a974
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-04-23 13:54:02 +0630

    api - route update

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProgramController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::apiResource('/programs', ProgramController::class);
