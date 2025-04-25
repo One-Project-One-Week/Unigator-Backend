b8c755d4d181c2b829fad86a6552451d22a51377
Khai Sian Taang <khaisian.khaisiantaang@gmail.com>
2025-04-25 14:30:48 +0630

    Program Filter Error

--- database/migrations/2025_04_23_041554_create_programs_table.php
+++ database/migrations/2025_04_23_041554_create_programs_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -22,6 +21,7 @@ return new class extends Migration
             $table->json('application_requirement');
             $table->string('intake');
             $table->string('level');
+            $table->decimal('average_cost', 8, 2)->nullable();
             $table->enum('payment_plan', ['monthly', 'per_semester', 'no_installements']);
             $table->timestamps();
         });
