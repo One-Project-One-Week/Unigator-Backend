83ca00191a8dee73dba33a566c671405d623e367
Nyan Oak Soe <nyanoaksoe77@gmail.com>
2025-04-27 04:13:47 +0630

    updated api routes

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,10 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/university/dashboard', [UniversityController::class, 'dashboard'])->middleware('university');
     Route::get('/user', [UserController::class, 'getUserProfile']);
     Route::put('/user', [UserController::class, 'updateProfile']);
+    Route::post('/rating', [RatingController::class, 'createRating']);
+    Route::post('/accomodation', [AccomodationController::class, 'createAccomodation']);
+
+
 });
 
 Route::apiResource('/programs', ProgramController::class);
@@ -40,7 +44,6 @@ Route::apiResource('/categories', CategoryController::class);
 Route::get('/avgFees', [ProgramController::class, 'getAverageProgramCost']);
 Route::get('/findProgram', [ProgramController::class, 'getPrograms']);
 
-Route::post('/accomodation', [AccomodationController::class, 'createAccomodation']);
 Route::get('/accomodation', [AccomodationController::class, 'getAllAccomodations']);
 
 
@@ -50,7 +53,6 @@ Route::delete('/accomodation/{id}', [AccomodationController::class, 'deleteAccom
 
 
 Route::get('/rating', [RatingController::class, 'getRating']);
-Route::post('/rating', [RatingController::class, 'createRating']);
 
 
 Route::get('/scholarship', [ScholarshipController::class, 'getScholarshipInfoAll']);
